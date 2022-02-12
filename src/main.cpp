173c182f5d54e6e7d95b5b3e28218a3c7f304c3a
Fred Emmott <git@fredemmott.com>
2022-02-12 15:45:15 -0600

    Some progress, some comments

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,51 @@
 // APIs for now,
 #include <MddBootstrapAutoInitializer.cpp>
 
+namespace muxc = winrt::Microsoft::UI::Xaml::Controls;
+
+struct MainWindow : public winrt::Microsoft::UI::Xaml::WindowT<MainWindow>
+{
+private:
+  muxc::NavigationView mNav;
+  muxc::Frame mContent;
+
+public:
+  MainWindow()
+  {
+    mNav.Header(winrt::box_value(L"Header"));
+    mNav.Content(mContent);
+    mNav.IsSettingsVisible(false);
+
+    muxc::NavigationViewItem item1;
+    item1.Content(winrt::box_value(L"Item 1"));
+    item1.Icon(muxc::SymbolIcon(muxc::Symbol::Play));
+
+    mNav.MenuItems().Append(item1);
+    // This fails due to Microsoft.UI.Xaml.Controls.XamlControlResources not
+    // being loaded, but loading it fails :(
+    Content(mNav);
+  }
+};
+
 struct MyApp : public winrt::Microsoft::UI::Xaml::ApplicationT<MyApp>
 {
+  MyApp()
+  {
+    RequestedTheme(winrt::Microsoft::UI::Xaml::ApplicationTheme::Dark);
+    UnhandledException([this](IInspectable const &, winrt::Microsoft::UI::Xaml::UnhandledExceptionEventArgs const &e)
+                       {
+        if (IsDebuggerPresent()) {
+            auto errorMessage = e.Message();
+            __debugbreak();
+        } });
+  }
+
   void OnLaunched(const winrt::Microsoft::UI::Xaml::LaunchActivatedEventArgs &)
   {
-    Resources().MergedDictionaries().Append(
-        winrt::Microsoft::UI::Xaml::Controls::XamlControlsResources());
+    // Exception thrown at 0x00007FFA07C74F69 (KernelBase.dll) in DemoApp.exe: WinRT originate error - 0x80004005 : 'Cannot find a resource with the given key: AcrylicBackgroundFillColorDefaultBrush.'.
+    muxc::XamlControlsResources x;
+    // ... if it werent' for that exception, I'd add it to `Resources()` here
+    winrt::make<MainWindow>().Activate();
   }
 };
 
@@ -25,6 +64,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
   winrt::init_apartment(winrt::apartment_type::single_threaded);
 
   winrt::Microsoft::UI::Xaml::Application::Start(
-    [](auto &&){ winrt::make<MyApp>(); });
+      [](auto &&)
+      { winrt::make<MyApp>(); });
   return 0;
 }
