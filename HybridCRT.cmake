95bd51525045bced2ba9ff8e767f90cdd163a022
Fred Emmott <git@fredemmott.com>
2022-06-15 18:24:29 -0500

    Hybrid CRT

---
+++ HybridCRT.cmake
@@ -0,0 +1,14 @@
+# Hybrid CRT
+# https://github.com/microsoft/WindowsAppSDK/blob/main/docs/Coding-Guidelines/HybridCRT.md
+
+# Enable CMAKE_MSVC_RUNTIME_LIBRARY variable
+cmake_policy(SET CMP0091 NEW)
+set(
+	CMAKE_MSVC_RUNTIME_LIBRARY
+	# Statically link the C++ runtime libraries, but partially override this below
+	"MultiThreaded$<$<CONFIG:Debug>:Debug>"
+)
+add_link_options(
+	"/DEFAULTLIB:ucrt$<$<CONFIG:Debug>:d>.lib" # include the static UCRT
+	"/NODEFAULTLIB:libucrt$<$<CONFIG:Debug>:d>.lib" # ignore the dynamic UCRT
+)
